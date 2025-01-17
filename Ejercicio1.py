8d4fbb78c8dad875bf98da93a9e56983885386ed
rebardo0 <svelabeltran@gmail.com>
2025-01-17 14:43:25 +0100

    En proceso

---
+++ Ejercicio1.py
@@ -0,0 +1,9 @@
+def mayor_edad():
+    edad = int(input("Introduzca su edad:"))
+
+    if edad >=18:
+        print("Si puedes votar.")
+    else:
+        print("No puedes votar.")
+    
+mayor_edad()
